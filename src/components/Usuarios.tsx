a0c1ea99a293a6ecffc8a9d007ce5da4ff07c8c8
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-04-17 00:44:23 -0500

    usando useState e interfaces para renderizar informacion de la API

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,16 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { reqResApi } from "../api/reqRes"
+import { ReqRespListado, Usuario } from '../interfaces/reqRes';
 
 export default function Usuarios() {
 
+  const [usuarios, setUsuarios] = useState<Usuario[]>([])
+
   useEffect(() => {
     // Haciendo el llamado a la API
-    reqResApi.get('/users')
+    reqResApi.get<ReqRespListado>('/users')
       .then(resp => {
-        
+        setUsuarios(resp.data.data)
       })
       .catch(err => console.log(err))
   }, [])
+
+  const renderItem = ({id, first_name, last_name, email, avatar}:Usuario) => {
+    return (
+      <tr key={id.toString()}>
+        <td>
+          <img src={avatar} alt={first_name} style={{width:35, borderRadius:50}}/>
+        </td>
+        <td>{first_name} {last_name}</td>
+        <td>{email}</td>
+      </tr>
+    )
+  }
 
   return (
     <div>
@@ -24,7 +39,9 @@ export default function Usuarios() {
           </tr>
         </thead>
         <tbody>
-
+          {
+            usuarios.map(renderItem)
+          }
         </tbody>
       </table>
     </div>
